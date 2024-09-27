eb5dfc38376d38d8e7c1d99c76eed21b2f31fcc3
onglityit <onglityit@users.noreply.github.com>
2024-09-27 15:20:26 +0800

    cache listener

--- healthchat/v0.1/service-worker.js
+++ healthchat/v0.1/service-worker.js
@@ -1,43 +1,44 @@
 const CACHE_NAME = 'healthchat-v1';
-const urlsToCache = [
-  'https://onglityit.github.io/healthchat/v0.1/',
-  'https://onglityit.github.io/healthchat/v0.1/index.html',
-  'https://onglityit.github.io/healthchat/v0.1/static/js/main.js',
-  'https://onglityit.github.io/healthchat/v0.1/static/css/main.css',
-  // Add other URLs you want to cache
-];
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+
+      return fetch(event.request).then((response) => {
+        // Check if we received a valid response
+        if (!response || response.status !== 200 || response.type !== 'basic') {
           return response;
         }
-        return fetch(event.request);
-      }
-    )
+
+        // Clone the response
+        const responseToCache = response.clone();
+
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+
+        return response;
+      });
+    })
   );
 });
 
-self.addEventListener('activate', function(event) {
-  var cacheWhitelist = ['healthchat-v1'];
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+
   event.waitUntil(
-    caches.keys().then(function(cacheNames) {
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map(function(cacheName) {
+        cacheNames.map((cacheName) => {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
             return caches.delete(cacheName);
           }
