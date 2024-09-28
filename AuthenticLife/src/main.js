7b73b69fff6b3f8742832022c328e8eda6c3861b
onglityit <onglityit@users.noreply.github.com>
2024-09-28 18:26:29 +0800

    v0.2

--- AuthenticLife/src/main.js
+++ AuthenticLife/src/main.js
@@ -1,8 +1,8 @@
 import { initVersionCheck } from './0-version/0-version.js';
-
+import { initIntro } from './00-intro/00-intro.js';
 
 $(document).ready(function() {
-    // Your existing initialization code...
+    initIntro();
 
     // Initialize version checking
     initVersionCheck();
